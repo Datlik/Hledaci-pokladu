9579cf46b30e3506ac65b6cce03f4e9d41bdae7c
Jakub Datel <126694823+Datlik@users.noreply.github.com>
2024-04-29 19:51:57 +0200

    Update __init__.py
    Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = os.urandom(24)
+
+    from .main import main
+    from .auth import auth
+
+    app.register_blueprint(main, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    return app
