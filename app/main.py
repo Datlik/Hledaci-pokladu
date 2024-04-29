e1c39a5d2606340a9bf34a13f475d4a7645dcdb9
VitBouska <126513574+VitBouska@users.noreply.github.com>
2024-04-29 19:55:05 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,3 +6,18 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+
+@main.route("/E-shop")
+def e_shop():
+    return render_template("e-shop.html")
+
+
+@main.route("/Kontakty")
+def kontakty():
+    return render_template("kontakty.html")
+
+
+@main.route("/Nálezy")
+def nalezy():
+    return render_template("nalezy.html")
