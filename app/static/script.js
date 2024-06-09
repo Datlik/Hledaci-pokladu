4386ea603220e9e4ccf6ebb719da5557af664244
Jakub Datel <126694823+Datlik@users.noreply.github.com>
2024-06-09 19:20:12 +0200

    vytvoření funkce pro  světlý režim script.js

--- app/static/script.js
+++ app/static/script.js
@@ -1,1 +1,4 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
 
