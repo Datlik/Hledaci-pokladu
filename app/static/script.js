91e7517fa08e05e1a9e0e70db1e40655bef58e5f
Jakub Datel <126694823+Datlik@users.noreply.github.com>
2024-06-09 19:21:38 +0200

    dokončení pro světlý režim script.js

--- app/static/script.js
+++ app/static/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleButton = document.getElementById('theme-toggle');
     const currentTheme = localStorage.getItem('theme');
-
+    
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+    }
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('light-mode');
+        let theme = 'light';
+        if (document.body.classList.contains('light-mode')) {
+            theme = 'dark';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
