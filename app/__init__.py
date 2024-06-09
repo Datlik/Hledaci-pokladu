761e026ed61df9b55412220e0c5577b9e50fe23a
Jakub Datel <126694823+Datlik@users.noreply.github.com>
2024-06-09 20:23:31 +0200

    použití csrf ochrany __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import os
+from flask_wtf import CSRFProtect
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.urandom(24)
+    csrf = CSRFProtect(app)
 
     from .main import main
     from .auth import auth
